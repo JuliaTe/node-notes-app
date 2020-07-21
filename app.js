4ff9895efa9a8da900995890c7e4a44e6b17f317
Julia Thea <yuliya.boyadjan@gmail.com>
2020-07-21 10:45:45 -0500

    npm init

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const add = require('./utils.js');
-const getNotes = require('./notes.js');
 
-const sum = add(4, -2)
 const notes = getNotes();
 
-console.log(sum);
 console.log(notes);
--- utils.js
+++
@@ -1,6 +0,0 @@
-console.log('utils.js');
-const name = 'Lora';
-const add = function (a, b) {
-  return a + b;
-}
-module.exports = add;
