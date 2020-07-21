579f86c0ed321f93d7ac832be7d2f11b4f635b99
Julia Thea <yuliya.boyadjan@gmail.com>
2020-07-21 15:26:36 -0500

    misc

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 const validator = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const notes = getNotes();
-console.log(notes);
 
-console.log(validator.isURL('juultutby.com'));
 
-const name = 'Jules';
-console.log(chalk.green.inverse.underline('Hello %s'), name);
-
-console.log(chalk.blue('Hello world!'));
-console.log(chalk.bold.underline.rgb(10, 100, 200)('Hello!'));
-console.log(chalk.bold.rgb(10, 100, 200)`Hello!`);
-console.log(chalk`{bold.rgb(10,100,200) Hello!}`);
